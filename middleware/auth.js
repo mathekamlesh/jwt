90db37791b4617005a77cef1754a1ee996d84cbe
Kamlesh Mathe <mathekamlesh@gmail.com>
2022-01-08 13:50:24 +0530

    Remove config and format code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,20 @@
 const jwt = require("jsonwebtoken");
 const config = process.env;
-const verifyToken = (req,res,next) => {
-  const token = req.body.token || req.query.token || req.headers["x-access-token"];
+const verifyToken = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
 
-  if(!token){
+  if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token,config.TOKEN_KEY);
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
     req.user = decoded;
-    console.log(req.user)
+    console.log(req.user);
   } catch (error) {
     return res.status(401).send("Invalid Token");
   }
   return next();
-}
+};
 
 module.exports = verifyToken;
